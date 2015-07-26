cbe9584389efe7f80fc2174048a2c010b793abfb
Michael Weinberg <web@mweinberg.com>
2015-07-26 16:22:15 -0400

    readme, codebook, code appears final

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# cleaningdataproject
+## Cleaning Data Project
+
+Included in this repository is an R script run_analysis.R. It performs all of the functions assigned in the cleaning data project, including:
+
+> Merges the training and the test sets to create one data set.
+> Extracts only the measurements on the mean and standard deviation for each measurement. 
+> Uses descriptive activity names to name the activities in the data set
+> Appropriately labels the data set with descriptive variable names. 
+> From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
+To use run_analysis.R, set the working directory to the directory that contains the Samsung data (e.g. it contains README.txt, etc) A codebook is available called [CodeBook.md](CodeBook.md) that summarizes the values in th
--- run_analysis.R
+++ run_analysis.R
@@ -1,26 +1,28 @@
+# This code must be run when the working directory is set to the directory
+# that contains the Samsung data (e.g. it contains README.txt, etc)
+# https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
+
 
 library(dplyr) # need this library
 
-# read in data
+# read in data- and label appropriately
 
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt", header = FALSE, col.names=c("activity", "activity_name"))
+activity_labels <- read.table("activity_labels.txt", header = FALSE, col.names=c("activity", "activity_name"))
 
-
-
-features <-read.table("UCI HAR Dataset/features.txt", header = FALSE)
+features <-read.table("features.txt", header = FALSE)
 # get indices of columns we want to examine
-cols_we_want <- grep("mean", features$V2, ignore.case=TRUE)
+cols_we_want <- grep("mean|std", features$V2, ignore.case=TRUE)
 saved_features <- features[cols_we_want, ] # saved for later
 
-test_set <-read.table("UCI HAR Dataset/test/X_test.txt", header = FALSE)
-test_activity <-read.table("UCI HAR Dataset/test/y_test.txt",  header = FALSE)
-test_subject <-read.table("UCI HAR Dataset/test/subject_test.txt",  header = FALSE)
+test_set <-read.table("test/X_test.txt", header = FALSE)
+test_activity <-read.table("test/y_test.txt",  header = FALSE)
+test_subject <-read.table("test/subject_test.txt",  header = FALSE)
 
-train_set <-read.table("UCI HAR Dataset/train/X_train.txt", header = FALSE)
-train_activity <-read.table("UCI HAR Dataset/train/y_train.txt" , header = FALSE)
-train_subject <-read.table("UCI HAR Dataset/train/subject_train.txt", header = FALSE)
+train_set <-read.table("train/X_train.txt", header = FALSE)
+train_activity <-read.table("train/y_train.txt" , header = FALSE)
+train_subject <-read.table("train/subject_train.txt", header = FALSE)
 
-# add column names to training and test data
+# add column names to training and test data- to complete STEP 4
 colnames(train_set) <- features[, 2]
 colnames(test_set) <- features[, 2]
 
@@ -33,8 +35,8 @@ colnames(train_activity) <- "activity"
 # combine test and train data into one big set- STEP 1 COMPLETE
 combined_sets <- rbind(test_set, train_set)
 
-# Extracts only the measurements on the mean and standard deviation for each measurement. 
-# STEP 2 COMPLETE
+# Extracts only the measurements on the mean
+# and standard deviation for each measurement. STEP 2 COMPLETE
 combined_sets_irrelevant_removed <-combined_sets[, cols_we_want]
 
 # combine activity test and train activity data
@@ -49,3 +51,12 @@ combined_sets_final <- cbind(combined_subject, combined_activity,combined_sets_i
 # add the activity names- STEP 3 COMPLETE
 # Uses descriptive activity names to name the activities in the data set
 labeled_table <- inner_join(activity_labels, combined_sets_final)
+
+# groups by activity and subject- also add in name which corresponds to activity
+grouped <- group_by(labeled_table, activity_name, activity, subject )
+
+# apply mean to get summary data
+final_output <- summarise_each(grouped, funs(mean(., na.rm = TRUE)))
+
+# output summary to file- STEP 5 COMPLETE
+write.table(final_output, file="summary_data_means.txt", row.names = FALSE)
